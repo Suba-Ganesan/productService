8caa77caed54fe5ec311a0cc3c9872fb61b8da27
suba <suba7g@gmail.com>
2022-10-05 10:11:20 +0530

    changes

--- ProductService/src/main/java/com/ProductService/exception/ProductNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.ProductService.exception;
-
-public class ProductNotFoundException extends RuntimeException{
-
-	public ProductNotFoundException(int id) {
-		super("Product with id " + id + " not found.");
-	}
-
-}
